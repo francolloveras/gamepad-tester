a64b5ee3b3488c834dcef1b4e7e1172b5368156f
Franco Lloveras <francorlloveras@gmail.com>
2026-02-19 22:09:22 -0300

    Change main layout

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from '@/App.tsx'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="flex flex-col items-center gap-y-12 min-h-screen">
+      <Header />
+      <div className="flex-1">
+        <App />
+      </div>
+      <Footer />
+    </div>
   </StrictMode>
 )
