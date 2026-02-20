0a073a9fa7ad1a3b0d5f8bd59f2b6ca965cebfff
Franco Lloveras <francorlloveras@gmail.com>
2026-02-20 00:13:54 -0300

    Add gamepad logic in the main app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import Dualsense from '@/components/Dualsense'
-import Loader from '@/components/icons/loader'
+import DisconnectedPanel from '@/components/disconnected-panel'
+import GamepadPanel from '@/components/gamepad-panel'
+import Dualsense from '@/components/dualsense'
+import { useGamepads } from '@/lib/hooks/useGamepads'
 
 export default function App() {
+  const gamepads = useGamepads()
+
   return (
-    <div className="flex items-start gap-x-24 rounded-lg border border-neutral-100 px-12 py-10 shadow-lg">
-      <div>
-        <h2 className="inline-flex items-center gap-x-2 text-2xl text-neutral-700">
-          <Loader />
-          Waiting for a controller to be connect
-        </h2>
-        <p className="text-neutral-500">Connect your joystick and press any button to start.</p>
-      </div>
-      <div id="gamepad-name"></div>
+    <div className="flex items-start gap-x-12 rounded-lg border border-neutral-100 px-12 py-10 shadow-lg">
+      {gamepads.length > 0 ? <GamepadPanel gamepad={gamepads[0]} /> : <DisconnectedPanel />}
       <div className="w-96">
         <Dualsense />
       </div>
