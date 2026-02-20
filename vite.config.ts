2307ff7d57b2a69aeabf94b132c6219058b6dbc8
Franco Lloveras <francorlloveras@gmail.com>
2026-02-19 21:53:55 -0300

    Add TypeScript path aliases config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
@@ -18,5 +19,10 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
