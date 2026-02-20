0427315100cd5590f55ec6b11a83d98c98f8f0e8
Franco Lloveras <francorlloveras@gmail.com>
2026-02-20 20:58:37 -0300

    Improve App main styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   const gamepads = useGamepads()
 
   return (
-    <div className="flex items-start gap-x-12 rounded-lg border border-neutral-100 px-12 py-10 shadow-lg">
+    <div className="flex items-start justify-between gap-x-8 rounded-lg border border-neutral-100 px-12 py-10 shadow-lg">
       {gamepads.length > 0 ? <GamepadPanel gamepad={gamepads[0]} /> : <DisconnectedPanel />}
       <div className="w-96">
         <Dualsense />
