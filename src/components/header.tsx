13305eab1995c0ad6bbacb0ddb2ec29902089bf5
Franco Lloveras <francorlloveras@gmail.com>
2026-02-19 22:08:35 -0300

    Add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="mt-12 text-center">
+      <h1 className="text-4xl">
+        <strong className="font-semibold text-neutral-900">Gamepad</strong>
+        <span className="font-light">Tester</span>
+      </h1>
+      <p className="text-xl text-neutral-600">Visualize gamepad input in real-time online!</p>
+    </header>
+  )
+}
