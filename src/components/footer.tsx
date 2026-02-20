e357e61c334ba5359dd77e69d85ea5adc107e6b6
Franco Lloveras <francorlloveras@gmail.com>
2026-02-19 22:08:50 -0300

    Add footer component with attribution mention

---
+++ src/components/footer.tsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+  return (
+    <footer className="mb-4 text-center">
+      <p className="text-sm text-neutral-600">&copy; 2026 Franco Lloveras</p>
+      <p className="text-xs text-neutral-500">
+        Vectors and icons by{' '}
+        <a
+          href="https://gumroad.com/darkwing?ref=svgrepo.com"
+          target="_blank"
+          referrerPolicy="no-referrer"
+        >
+          Alex Martynov
+        </a>
+        in CC Attribution License via
+        <a href="https://www.svgrepo.com/" target="_blank" referrerPolicy="no-referrer">
+          SVG Repo
+        </a>
+        .
+      </p>
+    </footer>
+  )
+}
