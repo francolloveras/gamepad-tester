3a6de909b43c13c7b6d0a70c2541613860085600
Franco Lloveras <francorlloveras@gmail.com>
2026-02-19 21:52:50 -0300

    Add Geist font

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import Unfonts from 'unplugin-fonts/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [
+    react(),
+    tailwindcss(),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: 'Geist',
+            src: './src/assets/fonts/Geist.ttf'
+          }
+        ]
+      }
+    })
+  ]
 })
